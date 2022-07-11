bb4dcfa22329f32891b77bdbfd5f257136098b4e
D-cun0205 <mmeo0205@gmail.com>
2022-07-11 17:17:52 +0900

    multiple account commit re test

--- src/App.js
+++ src/App.js
@@ -2,12 +2,13 @@ import './App.css';
 import Game from './pages/abc';
 
 function App() {
-  return (
-    <div className="App">
-      <header className="App-header" />
-      <Game />
-    </div>
-  );
+    console.log('진입');
+      return (
+            <div className="App">
+                  <header className="App-header" />
+                  <Game />
+            </div>
+      );
 }
 
 export default App;
